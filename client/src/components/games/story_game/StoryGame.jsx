643dc89bccdd90dd264cdf1c0b074992f23e18b7
joonanykanen <joona.nykanen@icloud.com>
2024-04-08 15:46:29 +0300

    Container initialisations

---
+++ client/src/components/games/story_game/StoryGame.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StoryGame = () => {
+    return (
+        <div>
+            {/* Your component content goes here */}
+        </div>
+    );
+};
+
+export default StoryGame;
