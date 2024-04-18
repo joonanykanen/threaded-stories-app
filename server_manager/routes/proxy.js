2442abb2d255a2665b287fad75d7a6ed62c8d369
Albert Cuny01~ <cunyalbert16@gmail.com01~>
2024-04-18 15:43:31 +0300

    serverScanner added, logic for choosing the server for client

--- server_manager/routes/proxy.js
+++ server_manager/routes/proxy.js
@@ -1,27 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
-
-const servers = [
+// in the future just host and port number
+// just a list that lists the necessary requests to be made
+var servers = [
     {
         host: 'localhost',
-        port: 3000,
-        users: 1,
+        port: 3001,
     },
     {
         host: 'localhost',
         port: 8080,
-        users: 2,
     },
     {
         host: 'localhost',
         port: 5000,
-        users: 2,
     },
     {
         host: 'localhost',
         port: 1234,
-        users: 3,
     },
     {
         host: 'localhost',
@@ -31,29 +29,82 @@ const servers = [
     {
         host: 'localhost',
         port: 7070,
-        users: 4,
     }
-    // Add more server entries as needed
+    
 ];
 
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
     try {
+
         var mostUsers = 0;
-        var forwardedServer;
-        servers.forEach( (server) => {
+        const forwardedServer = await serverScanner();
+
+       /* servers.forEach( (server) => {
+
             if(server.users > mostUsers) {
                 mostUsers = server.users
                 forwardedServer = server
             }
-        })
+        })*/
 
-        // Code that may potentially throw an error
+        
         res.send(forwardedServer);
     } catch (error) {
-        // If an error occurs, handle it here
-        next(error); // Pass the error to the next middleware or error handler
+        
+        next(error); 
     }
 })
+
+// loops through making requests to each server for health checks and user number updates
+// returns server that has the most users under 5 users
+const serverScanner = async () => {
+    console.log("hei hei")
+    // 
+    let mostUsers = 0;
+    let forwardedServer;
+    const results = []; 
+    for (let i = 0; i < servers.length; i++) {
+        let server = servers[i]
+
+        try {
+            const response = await axios.get(`http://${server.host}:${server.port}/app/healthcheck`);
+            // Check status
+            if (response.status === 200) {
+              results.push({
+                id: server.port,
+                users: response.data.users,
+                status: 'passing'
+              });
+              users = response.data.users
+              if(users > mostUsers & users < 5){
+                mostUsers = users
+                forwardedServer = server
+              }
+              
+            } else {
+              results.push({
+                id: server.port,
+                users: server.data.users,
+                status: 'failing' 
+              });
+            }
+            
+          } catch (error) {
+            results.push({
+              id: server.port,
+              users: server.data.users,
+              status: 'failing'
+            });
+          }
+        
+
+    }
+
+    console.log(results)
+
+    return forwardedServer
+
+}
 
 module.exports = router;
