736f3e3b592ddfbede81d22a72feecd08c59e3be
JoonaManninen <joona.manninen7@gmail.com>
2024-04-18 15:16:28 +0300

    Game component rendering when queue full

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import NicknameInput from './components/nickname/NicknameInput';
 import MainMenu from './components/main_menu/MainMenu';
 import Queue from './components/queue/Queue';
+import Storygame from './components/games/story_game/StoryGame';
 import socketIOClient from 'socket.io-client';
 
 const ENDPOINT = 'http://127.0.0.1:4001';
@@ -10,6 +11,7 @@ function App() {
   const [nickname, setNickname] = useState('');
   const [inQueue, setInQueue] = useState(false);
   const [socket, setSocket] = useState(null);
+  const [storygame, setStorygame] = useState(null);
 
   const handleNicknameSubmit = (newNickname) => {
     setNickname(newNickname);
@@ -28,8 +30,9 @@ function App() {
   return (
     <div className="App">
       {!nickname && <NicknameInput onSubmit={handleNicknameSubmit} />}
-      {nickname &&!inQueue && <MainMenu onQueue={handleQueue} nickname={nickname} />}
-      {nickname && inQueue && <Queue inQueue={inQueue} setInQueue={setInQueue} socket={socket} />}
+      {nickname &&!inQueue && !storygame &&<MainMenu onQueue={handleQueue} nickname={nickname} />}
+      {nickname && inQueue && <Queue inQueue={inQueue} setInQueue={setInQueue} socket={socket} setStorygame={setStorygame} />}
+      {nickname && !inQueue && storygame && <Storygame inQueue={inQueue} setStorygame={setStorygame} socket={socket} />}
     </div>
   );
 }
--- client/src/components/queue/Queue.jsx
+++ client/src/components/queue/Queue.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import './Queue.css';
 
-const Queue = ({ inQueue, setInQueue, socket }) => {
+
+const Queue = ({ inQueue, setInQueue, socket, setStorygame }) => {
   const cancelQueue = () => {
     setInQueue(false);
     socket.disconnect();
@@ -11,6 +12,7 @@ const Queue = ({ inQueue, setInQueue, socket }) => {
     socket.on('game-start', () => {
       setInQueue(false);
       console.log("peli alkaa");
+      setStorygame(true);
     });
   }, [socket, setInQueue ]);
 
