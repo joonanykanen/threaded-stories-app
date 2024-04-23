f378607da206cd798f3da3b4f6f1d25653a3684d
Xijaxa <81866800+Xijaxa@users.noreply.github.com>
2024-04-23 16:06:56 +0300

    Game base

--- server/games/story_game/app.js
+++ server/games/story_game/app.js
@@ -8,6 +8,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +18,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('/storygame', () => {
+    var story = [];
+    var turn = 1;
+    var newWord = "";
+    var wordChoices = [];
+    var rounds = 0;
+    while(true) {
+        // fetch words from DB
+        // send to other users the current story and tell them its not their turn
+        // send words and current story to user, get the new word
+        story.push(newWord);
+        turn += 1;
+        if(turn == 6) {
+            turn = 1;
+            rounds += 1;
+            if(rounds == 5) {
+                break;
+            }
+        }
+    }
+    //push story to DB
+    //send game omer
+})
 
 module.exports = app;
