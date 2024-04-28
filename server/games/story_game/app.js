07da1fccaaed49d88cc0d3b57cd9fbb471032d90
Joona <111046007+joonanykanen@users.noreply.github.com>
2024-04-28 15:33:11 +0300

    Revert "THE_GAME"

--- server/games/story_game/app.js
+++ server/games/story_game/app.js
@@ -2,16 +2,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const http = require('http');
-const socketIo = require('socket.io');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dbhandler = require('../database_handler/routes/database_manager.js');
 
 var app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,105 +18,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-const PORT = process.env.PORT || 3000;
-
-const playercount = 5;
-const totalrounds = 3;
-var story = "";
-var turn = 0;
-var rounds = 0;
-var nicknames = [];
-var players = [];
-
-io.on('connection', async (socket) => {
-  console.log('A user connected');
-  
-  // Handle getting nicknames
-  socket.on('give-nickname', (nickname) => {
-    if (players.length < playercount) {
-        players.push(socket.id);
-        nicknames.push(nickname);
-        if (players.length === playercount) {
-            io.emit('game-start', { players: nicknames });
-            startGame();
+app.get('/storygame', () => {
+    var story = "";
+    var turn = 0;
+    var newWord = "";
+    var wordChoices = [];
+    var rounds = 0;
+    var nicknames = [];
+    while(true) {
+        // fetch words from DB
+        
+        // send to other users the current story and tell them its not their turn
+        // send words and current story to user, get the new word
+        story.push(newWord);
+        turn += 1;
+        if(turn == 5) {
+            turn = 0;
+            rounds += 1;
+            if(rounds == 5) {
+                break;
+            }
         }
-      }
-    });
+    }
+    //push story to DB
     
-    socket.on('submit-word', (word) => {
-      if (socket.id === players[turn]) {
-          story += " " + word;
-          turn = (turn + 1) % playerCount;
-          if (turn === 0) {
-              rounds++;
-              if (rounds === totalRounds) {
-                  endGame();
-              } else {
-                  startGame();
-              }
-          }
-      }
-  });
-
-  socket.on('disconnect', () => {
-    const index = players.indexOf(socket.id);
-    if (index !== -1) {
-        players.splice(index, 1);
-        nicknames.splice(index, 1);
-    }
-  });
-});
-
-async function startGame() {
-  // fetch words from DB
-  try {
-    const response = await fetch("http://localhost:3000/database/mixed-words", {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json"
-      },
-    });
-    const wordChoices = response.json();
-    // send to other users the current story and tell whose turn it is
-    io.emit('story', { story, player: nicknames[turn]});
-    // send words and current story to user, get the new word
-    io.emit('give-words', wordChoices);
-    } catch (error) {
-      console.error('Error fetching words:', error);
-  }
-}
-  
-
-async function endGame() {
-  const title = nicknames[0] + " & friends story";
-    try {
-      //push story to DB
-      await fetch("http://localhost:3000/database/stories", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: {
-          "title": title,
-          "content": story
-        }
-      })
-      //send game over
-      io.emit('game-over', story);
-      
-      story = "";
-      turn = 0;
-      rounds = 0;
-      nicknames = [];
-      players = [];
-    } catch (error) {
-        console.error('Error posting story:', error);
-    }
-}
-
-server.listen(PORT, () => {
-  console.log('Server is running on port 3000');
-});
+    //send game omer
+})
 
 module.exports = app;
